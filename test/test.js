1b3850723a1a102e2259abda6d08425d2053e353
Weerasak Chongnguluam <singpor@gmail.com>
2015-03-10 14:28:21 +0700

    remove $apply

--- test/test.js
+++ test/test.js
@@ -11,14 +11,12 @@ describe("Test register broadcast message", function() {
 
     it("Should set message to Hello", inject(function(MessageService, $rootScope) {
         $rootScope.$broadcast('Message', 'Hello');
-        $rootScope.$apply();
 
         expect(MessageService.message).toEqual('Hello');
     }));
 
     it("Should set message to สวัสดี", inject(function(MessageService, $rootScope) {
         $rootScope.$broadcast('Message', 'สวัสดี');
-        $rootScope.$apply();
 
         expect(MessageService.message).toEqual('สวัสดี');
     }));
